1b325e4ada6df3deeffa3641be7cfe6cebf322fa
Sean Duggan <sean.duggan@gmail.com>
2025-06-02 07:44:06 -0400

    Improved the UI a bit

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,14 +1,31 @@
-﻿using UnityEngine;
+﻿using QFSW.QC;
+using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
 
 class GameManager : MonoBehaviour
 {
     [SerializeField] Transform _enemyGoalPoint;
-    [SerializeField] int _currency;
-
+    
     public int Cash { get { return _currency; } }
     public Transform EnemyGoalPoint { get { return _enemyGoalPoint; } }
 
     public static GameManager Instance { get; private set; }
+
+    [SerializeField] int _currency;
+    [SerializeField] Text _currencyText;
+
+    [SerializeField] int _lives;
+    [SerializeField] Text _livesText;
+
+    [SerializeField] int _waveNum;
+    [SerializeField] Text _waveText;
+
+    [SerializeField] string _version = "1.0";
+    [SerializeField] Text _versionText;
+
+    [SerializeField] Text _status;
 
     private void Awake()
     {
@@ -19,6 +36,16 @@ class GameManager : MonoBehaviour
             return;
         }
         Instance = this;
+
+        SetInitialText();
+    }
+
+    private void SetInitialText()
+    {
+        _currencyText.text = _currency.ToString();
+        _waveText.text = _waveNum.ToString();
+        _livesText.text = _lives.ToString();
+        _versionText.text = _version;
     }
 
     public void SetTimeScale(float timeScale)
@@ -26,9 +53,40 @@ class GameManager : MonoBehaviour
         Time.timeScale = timeScale;
     }
 
+    [Command]
     public void ModifyCash(int amount)
     {
         _currency += amount;
-        // Update the UI
+        _currencyText.text = _currency.ToString();
     }
+
+    [Command]
+    public void ModifyLives(int amount)
+    {
+        _lives += amount;
+        _livesText.text = _lives.ToString();
+
+        if (_lives > 60) {
+            _status.text = "Good";
+            _status.color = Color.blue;
+        }
+        else if (_lives > 20)
+        {
+            _status.text = "So-So";
+            _status.color = Color.yellow;
+        } else
+        {
+            _status.text = "Dire";
+            _status.color = Color.red;
+        }
+
+    }
+
+    [Command]
+    public void SetWaveNumber(int number)
+    {
+        _waveNum = number;
+        _waveText.text = _waveNum.ToString();
+    }
+
 }
