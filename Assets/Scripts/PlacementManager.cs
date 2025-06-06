54f4a22806e86911c6291b3b93863c5a415bdc57
Sean Duggan <sean.duggan@gmail.com>
2025-06-06 15:54:25 -0400

    I think things are changed back?

--- Assets/Scripts/PlacementManager.cs
+++ Assets/Scripts/PlacementManager.cs
@@ -1,8 +1,8 @@
 using CodeMonkey.Toolkit.TMousePosition;
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PlacementManager : MonoBehaviour
 {
@@ -19,7 +19,7 @@ public class PlacementManager : MonoBehaviour
             if (_isValid != value)
             {
                 // Change shader
-                foreach (Renderer renderer in _placementObject.GetComponents<Renderer>())
+                foreach (Renderer renderer in GetComponents<Renderer>())
                 {
                     foreach (Material material in renderer.materials)
                     {
@@ -27,7 +27,7 @@ public class PlacementManager : MonoBehaviour
                     }
                 }
 
-                foreach (Renderer renderer in _placementObject.GetComponentsInChildren<Renderer>())
+                foreach (Renderer renderer in GetComponentsInChildren<Renderer>())
                 {
                     foreach (Material material in renderer.materials)
                     {
@@ -39,75 +39,48 @@ public class PlacementManager : MonoBehaviour
         } 
     }
 
-    [SerializeField] private LayerMask _mouseColliderLayerMask;
+        _placementPrefab = incoming;
 
     void Start()
     {
-        if (_placementPrefab != null)
-        {
-            SetPlacementObject(_placementPrefab);
-        }
+        _placementObject = Instantiate(_placementPrefab);
     }
 
-    public void SetPlacementObject(Emplacement incoming)
+    public void SetPlacementObject(GameObject placementObject)
     {
-        Debug.Log("Attempting to change placement object to " + incoming);
-        if (_placementObject)
-        {
-            Destroy(_placementObject.gameObject);
-        }
-
-        _placementPrefab = incoming;
-
-        if (incoming == null)
-        {
-            Debug.Log("Null set");
-            return;
-        }
-
-        Debug.Log("Setting placement object to " + incoming.name, incoming);
-        _placementObject = Instantiate(_placementPrefab);
+        _placementObject = placementObject;
     }
 
     // Update is called once per frame
     void Update()
     {
+        PlacementObject placementPoint = null;
         if (_placementObject != null)
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out RaycastHit raycastHit, float.MaxValue, _mouseColliderLayerMask))
             {
-                GameObject target = raycastHit.collider.gameObject;
-                PlacementObject targetObject = target.GetComponent<PlacementObject>();
-                IsValid = DeterminePlacementValidity(targetObject);
+                IsValid = DeterminePlacementValidity();
                 _placementObject.transform.position = raycastHit.collider.gameObject.transform.position;                
             }
             else
             {
-                IsValid = false;
                 _placementObject.transform.position = MousePositionPlane.GetPosition();
+                _isValid = false;
             }
 
-            if (Input.GetMouseButtonDown(0))
+            if (!EventSystem.current.IsPointerOverGameObject() && Input.GetMouseButtonDown(0))
             {
-                if (_isValid)
+                if (_isValid && placementPoint != null)
                 {
-                    Emplacement placed = Instantiate(_placementPrefab, _placementObject.transform.position, Quaternion.identity);
-                    placed.SetActive(true);
-                    GameManager.Instance.ModifyCash(-_placementPrefab.Cost);
+                    Instantiate(_placementPrefab, _placementObject.transform.position, Quaternion.identity);
                 }
-            }
-
-            if (Input.GetMouseButtonDown(1))
-            {
-                SetPlacementObject(null);
-                // Contact UI Manager to un-highlight?
             }
         }
     }
 
-    private bool DeterminePlacementValidity(PlacementObject targetObject)
+    private bool DeterminePlacementValidity()
     {
-        return (targetObject != null && targetObject.TurretObject == null && _placementObject.Cost <= GameManager.Instance.Cash);
+        throw new NotImplementedException();
     }
 }
