2976f1886018d577b0558f079fa64ab4ef3b4e7f
Sean Duggan <sean.duggan@gmail.com>
2025-06-06 15:54:25 -0400

    I think things are changed back?

--- Assets/Scripts/PlacementManager.cs
+++ Assets/Scripts/PlacementManager.cs
@@ -1,85 +1,86 @@
 using CodeMonkey.Toolkit.TMousePosition;
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PlacementManager : MonoBehaviour
 {
-    [SerializeField] GameObject _placementPrefab;
+    [SerializeField] Emplacement _placementPrefab;
 
     [SerializeField] Shader _invalidEffect;
     [SerializeField] Shader _validEffect;
 
-    GameObject _placementObject;
+    Emplacement _placementObject;
     
     bool _isValid;
-    public bool IsValid { get { return _isValid; } 
-        set { 
-            if (_isValid != value)
-            {
-                // Change shader
-                foreach (Renderer renderer in GetComponents<Renderer>())
-                {
-                    foreach (Material material in renderer.materials)
-                    {
-                        material.shader = value ? _validEffect : _invalidEffect;
-                    }
-                }
-
-                foreach (Renderer renderer in GetComponentsInChildren<Renderer>())
-                {
-                    foreach (Material material in renderer.materials)
-                    {
-                        material.shader = value ? _validEffect : _invalidEffect;
-                    }
-                }
-            }
-            _isValid = value; 
-        } 
-    }
-
     [SerializeField] private LayerMask _mouseColliderLayerMask;
 
     void Start()
     {
-        _placementObject = Instantiate(_placementPrefab);
+        if (_placementPrefab != null)
+        {
+            _placementObject = Instantiate(_placementPrefab);
+        }
     }
 
-    public void SetPlacementObject(GameObject placementObject)
+    public void SetPlacementObject(Emplacement incoming)
     {
-        _placementObject = placementObject;
+        Debug.Log("Attempting to change placement object to " + incoming);
+        if (_placementObject)
+        {
+            Destroy(_placementObject.gameObject);
+        }
+
+        _placementPrefab = incoming;
+
+        if (incoming == null)
+        {
+            Debug.Log("Null set");
+            return;
+        }
+
+        Debug.Log("Setting placement object to " + incoming.name, incoming);
+        _placementObject = Instantiate(_placementPrefab);
     }
 
     // Update is called once per frame
     void Update()
     {
+        PlacementObject placementPoint = null;
         if (_placementObject != null)
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out RaycastHit raycastHit, float.MaxValue, _mouseColliderLayerMask))
             {
-                IsValid = DeterminePlacementValidity();
-                _placementObject.transform.position = raycastHit.collider.gameObject.transform.position;                
+                if (raycastHit.collider.gameObject.TryGetComponent<PlacementObject>(out placementPoint))
+                {
+                    _placementObject.transform.position = raycastHit.collider.gameObject.transform.position;
+                    _isValid = placementPoint.TurretObject == null && GameManager.Instance.Cash >= _placementObject.Cost;
+                }
             }
             else
             {
-                IsValid = false;
                 _placementObject.transform.position = MousePositionPlane.GetPosition();
+                _isValid = false;
             }
 
-            if (Input.GetMouseButtonDown(0))
+            if (!EventSystem.current.IsPointerOverGameObject() && Input.GetMouseButtonDown(0))
             {
-                if (_isValid)
+                if (_isValid && placementPoint != null)
                 {
-                    Instantiate(_placementPrefab, _placementObject.transform.position, Quaternion.identity);
+                    Emplacement placed = Instantiate(_placementPrefab, _placementObject.transform.position, Quaternion.identity);
+                    placementPoint.TurretObject = placed;
+                    GameManager.Instance.ModifyCash(-_placementObject.Cost);
+                    placed.SetActive(true);
                 }
             }
-        }
-    }
 
-    private bool DeterminePlacementValidity()
-    {
-        throw new NotImplementedException();
+            if (Input.GetMouseButtonDown(1))
+            {
+                SetPlacementObject(null);
+                // Contact UI Manager to un-highlight?
+            }
+        }
     }
 }
